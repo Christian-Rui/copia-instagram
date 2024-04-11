087fea82c8f0de6f804b43da163fa1bfc6191ba5
Christian Rui <chris2w0social@gmail.com>
2024-04-11 09:43:46 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import ConteudoLateralEsquerda from './ConteudoLateralEsquerda';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ConteudoLateralEsquerda/>
+      <div className='conteudoCentral'>
+        {/* Logo do instagram
+        Menu de navegação */}
+      </div>
+      <div className='conteudoLateralDireita'>
+        {/* 
+        Login
+        Sugestões de amigos
+        Sugestões de contas 
+        */}
+
+      </div>
+    </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/ConteudoLateralEsquerda.js
@@ -0,0 +1,11 @@
+
+export default function ConteudoLateralEsquerda() {
+    return (
+        <div className='conteudoLateralEsquerda'>
+            Exemplo de conteúdo lateral esquerda
+        </div>
+    )
+
+}
+
+// export default ConteudoLateralEsquerda;
