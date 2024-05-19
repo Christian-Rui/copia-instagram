bb8d87039b9666d469a167c355cac273a98f6223
Christian Rui <chris2w0social@gmail.com>
2024-05-19 15:58:04 -0300

    Organizando código

--- src/components/Direito/ConteudoLateralDireita.js
+++ src/components/Direito/ConteudoLateralDireita.js
@@ -69,37 +69,5 @@ export default function ConteudoLateralDireita() {
                  <div>© 2024 BEARTAGRAM FROM BEARLY</div>
             </footer>
         </div>
-
-
-        // <div className='Direita'>
-        //     <div className='topPerfil'>
-        //         <div className='userPerfil'>
-        //             {/* <img src={perfil}/> */}
-        //             <div>
-        //                 <p>therealpaleking</p>
-        //                 <p>Hollow Knight</p>
-        //             </div>
-        //         </div>
-        //         <p>Mudar</p>
-        //     </div>
-        //     <div className='suggestPefil'>
-        //         <p>Sugestões para você</p>
-        //         <p>Ver tudo</p>
-        //     </div>
-        //     <Suggestions/>
-        //     <div className='footer'>
-        //         <a>Sobre</a>
-        //         <a>Ajuda</a>
-        //         <a>Imprensa</a>
-        //         <a>API</a>
-        //         <a>Carreiras</a>
-        //         <a>Privacidade</a>
-        //         <a>Termos</a>
-        //         <a>Localizações</a>
-        //         <a>Idioma</a>
-        //         <a>Hallownest Verified</a>
-        //         <div>© 2024 NESTAGRAM FROM HALLOWNEST</div>
-        //     </div>
-        // </div>
     )
 }
